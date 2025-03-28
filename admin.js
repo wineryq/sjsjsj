7e66800625805a58fd53d584a0712c65be586934
Winery <katlawinery@gmail.com>
2025-03-28 21:24:39 +0300

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,38 @@
+const adminPasswordsFromJS = {
+  "BirSabahsizGece": {
+    name: "Umut Y.",
+    email: "katlawinery@gmail.com", // Geçici e-posta adresi
+    showMembers: true,
+    showSupport: true,
+    showServer: true,
+    showSponsor: true,
+    showAdmins: true
+  },
+  "BirSabahAhmet": {
+    name: "Ahmet K.",
+    email: "ahmet.test@tempmail.com", // Geçici e-posta adresi
+    showMembers: true,
+    showSupport: true,
+    showServer: true,
+    showSponsor: true,
+    showAdmins: true
+  },
+  "KeremBuA11e": {
+    name: "Kerem K.",
+    email: "kerem.test@tempmail.com", // Geçici e-posta adresi
+    showMembers: false,
+    showSupport: true,
+    showServer: false,
+    showSponsor: false,
+    showAdmins: false
+  },
+  "QlnKqmsjS2e": {
+    name: "Sefa B.",
+    email: "sefa.test@tempmail.com", // Geçici e-posta adresi
+    showMembers: false,
+    showSupport: true,
+    showServer: false,
+    showSponsor: false,
+    showAdmins: false
+  }
+};
---
+++ script.js
@@ -0,0 +1,8 @@
+// Updated event listeners for both buttons
+document.querySelector('.add-bot').addEventListener('click', function() {
+  window.location.href = 'https://discord.com/oauth2/authorize?client_id=1337408250995408897';
+});
+
+document.querySelector('.support').addEventListener('click', function() {
+  window.location.href = 'https://discord.gg/hydie';
+});
