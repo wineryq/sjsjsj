b442e8ee9efcad10fe3050dc4ef69f1e801bc6ed
Winery <katlawinery@gmail.com>
2025-03-29 22:16:19 +0300

    Update kupon.js

--- kupon.js
+++ kupon.js
@@ -1,5 +1,5 @@
 const coupons = {
-    "minehost25": { discount: 30, description: "deneme", type: "vds" },      
-    "domainhost25": { discount: 20, description: "test123", type: "domain" },  
+    "bayram25": { discount: 25, description: "deneme", type: "vds" },      
+    "domainhost25": { discount: 0, description: "test123", type: "domain" },  
     "000000001010010101001": { discount: 0, description: "indirim30", type: "both" }   
 };
