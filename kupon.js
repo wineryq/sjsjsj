7f7a9ca125bf2440c1854cdd35c2f5bdcfc61238
Winery <katlawinery@gmail.com>
2025-04-05 17:27:49 +0300

    Delete kupon.js

--- kupon.js
+++
@@ -1,5 +0,0 @@
-const coupons = {
-    "bayram25": { discount: 25, description: "deneme", type: "vds" },      
-    "domainhost25": { discount: 0, description: "test123", type: "domain" },  
-    "000000001010010101001": { discount: 0, description: "indirim30", type: "both" }   
-};
